7328aec8f5bded009fe1ef04f9089c1523a7085b
xd-Abi <abinayansureskumar@gmail.com>
2023-12-22 21:45:26 +0100

    Removed cpp project

--- setup.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-py setup.py
-call .target\premake\win64\premake5.exe vs2022
-PAUSE
--- source/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <astra/application.h>
-
-int main(int, char**)
-{
-    auto app = new Astra::Application();
-    app->Run();
-    delete app;
-}
